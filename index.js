fd8be1d75d9f57f0f5084b4f5a6fd0b48ad907a2
iperez-2024003 <iperez-2024003@kinal.edu.gt>
2026-03-14 21:55:55 -0600

    feat: servidor principal y colección de Postman

---
+++ index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import { initServer } from './configs/app.js';
+
+dotenv.config();
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', err);
+  process.exit(1);
+});
+
+initServer();
